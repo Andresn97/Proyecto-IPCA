4912ea86536b30df2c1df70a37b409d4ee5042cc
Andresn97 <andres97n@outlook.com>
2020-07-18 18:56:21 -0500

    Página de Inicio

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { InicioComponent } from "./components/inicio/inicio.component";
+import { CreacionPlanComponent } from "./components/creacion-plan/creacion-plan.component";
+
+const routes: Routes = [
+  {
+    path: "inicio",
+    component: InicioComponent,
+  },
+  {
+    path: "creacio-plan",
+    component: CreacionPlanComponent,
+  },
+  {
+    path: "**",
+    redirectTo: "/inicio",
+    pathMatch: "full",
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/components/creacion-plan/creacion-plan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-creacion-plan',
+  templateUrl: './creacion-plan.component.html',
+  styleUrls: ['./creacion-plan.component.css']
+})
+export class CreacionPlanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
