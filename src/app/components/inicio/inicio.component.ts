8c5fd8d126528a12ee9fc1c02fe427984fe90e09
Andresn97 <andres97n@outlook.com>
2020-07-21 21:31:22 -0500

    Cambios

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -16,6 +16,11 @@ export class InicioComponent implements OnInit {
       { name: "Book3", author: "Author3" },
       { name: "Book4", author: "Author4" },
       { name: "Book5", author: "Author5" },
+      { name: "Libro1", author: "Author1" },
+      { name: "Libro2", author: "Author2" },
+      { name: "Libro3", author: "Author3" },
+      { name: "Libro4", author: "Author4" },
+      { name: "Libro5", author: "Author5" },
     ];
   }
 
